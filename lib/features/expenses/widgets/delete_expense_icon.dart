4d21d86015c9db5ea0f95a40c656237462336d3c
ShuraShipai <shurashipai05@gmail.com>
2026-07-02 16:41:38 +0530

    Split delete confirmation widgets

---
+++ lib/features/expenses/widgets/delete_expense_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/theme/app_colors.dart';
+
+class DeleteExpenseIcon extends StatelessWidget {
+  const DeleteExpenseIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: AppColors.dangerSurface,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const SizedBox.square(
+        dimension: 60,
+        child: Icon(
+          Icons.delete_outline_rounded,
+          color: AppColors.danger,
+          size: 28,
+        ),
+      ),
+    );
+  }
+}
